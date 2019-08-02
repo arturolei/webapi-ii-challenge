04e9b805762511b24c6eaae6747001e0773728df
arturolei <artie.lei@gmail.com>
2019-08-01 19:25:09 -0700

    Modified knexfile for heroku postgres deploy

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,14 @@ module.exports = {
     },
     seeds: { directory: './data/seeds' },
   },
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }}
 };
